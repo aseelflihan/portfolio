fb398311721179b830c5b199bb91dabfca5f15b3
aseelflihan <aseelflihan@gmail.com>
2023-07-19 02:06:24 +0300

    google analytic view vistors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize('G-VX9X4V94K0');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
- 
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -16,13 +21,12 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-      
+
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
-      
     </BrowserRouter>
   );
 }
