2a1d12e5b9d22c01f994cef3195eaa57058d0cfa
aseelflihan <aseelflihan@gmail.com>
2023-07-20 23:33:31 +0300

    fix GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-const App = () => {
-  useEffect(() => {
-    ReactGA.initialize('G-VX9X4V94K0');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+ReactGA.initialize('G-VX9X4V94K0'); 
 
+const PageView = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
+  return null;
+};
+
+const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
+      <PageView />
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-        <></>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
+        <>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
